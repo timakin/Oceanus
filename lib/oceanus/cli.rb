35d70b9c1fc65374187d4984a78f9b3d9ea4d8d1
timakin <timaki.st@gmail.com>
2015-08-22 16:22:44 +0900

    [add] constants setting for command descriptions

--- lib/oceanus/cli.rb
+++ lib/oceanus/cli.rb
@@ -1,9 +1,11 @@
 require 'thor'
+require 'oceanus/constants'
 require 'oceanus/commands/pull'
 
 module Oceanus
     class CLI < Thor
-        desc "oceanus pull {image_name} (e.g. oceanus pull centos)", "Pull image to build a container"
+        include Constants 
+        desc *DESCRIPTIONS[:pull]
         def pull(image)
             Oceanus::Commands::Pull.exec(image)
         end
---
+++ lib/oceanus/constants.rb
@@ -0,0 +1,7 @@
+module Oceanus
+    module Constants
+        DESCRIPTIONS = {
+            pull: ["pull {image_name} (e.g. oceanus pull centos)", "Pull image to build a container"]
+        }
+    end
+end
