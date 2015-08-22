14f26b81b12fca311ae60f326b33e1cd734a7c30
timakin <timaki.st@gmail.com>
2015-08-22 18:40:43 +0900

    [add] comment for overview of pull command workflow

--- lib/oceanus/commands/pull.rb
+++ lib/oceanus/commands/pull.rb
@@ -2,6 +2,7 @@ module Oceanus
     module Commands
         class Pull
             def self.exec(image)
+                # http://docs.docker.com/v1.7/reference/api/hub_registry_spec/#pull
                 puts "pull #{image}"
             end
         end
