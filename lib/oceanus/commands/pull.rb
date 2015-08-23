922a69c3467d82b847f252abfd06c9d08acb3fd6
timakin <timaki.st@gmail.com>
2015-08-23 14:46:28 +0900

    [add] get_latest_tag

--- lib/oceanus/commands/pull.rb
+++ lib/oceanus/commands/pull.rb
@@ -5,9 +5,11 @@ module Oceanus
         class Pull
             def self.exec(image)
                 # http://docs.docker.com/v1.7/reference/api/hub_registry_spec/#pull
-                api = Oceanus::Utils::API.new
-                access_token = api.get_access_token('timakin/node-mongo-codemagnet')
+                api = Oceanus::Utils::API.new('timakin/node-mongo-codemagnet')
+                access_token = api.get_access_token
                 puts access_token
+                tag = api.get_latest_tag
+                puts tag
                 puts "pull #{image}"
             end
         end
--- lib/oceanus/utils/api.rb
+++ lib/oceanus/utils/api.rb
@@ -3,15 +3,22 @@ require 'httpclient'
 module Oceanus
     module Utils
         class API
-            def initialize
+            def initialize(image, tag='latest')
                 @client = HTTPClient.new()
-                @docker_hub_repository_path = "https://index.docker.io/v1/repositories"
+                @index_path = "https://index.docker.io/v1"
+                @registry_path = "https://registry-1.docker.io/v1"
+                @image_path = "#{@index_path}/repositories/#{image}/images"
+                @latest_tag_path = "#{@registry_path}/repositories/#{image}/tags/#{tag}"
             end
 
-            def get_access_token(image)
-                @image_path = "#{@docker_hub_repository_path}/#{image}/images"
+            def get_access_token
                 res = @client.get(@image_path, nil, {'X-Docker-Token' => true})
                 @access_token = res.header["X-Docker-Token"]
+            end
+
+            def get_latest_tag
+                res = @client.get(@latest_tag_path, nil, {'Authorization' => "Token #{@access_token.join(", ")}"})
+                @latest_tag = res.body
             end
         end
     end
