50f24e1a53d415b7560d933b3d837731d745280a
timakin <timaki.st@gmail.com>
2016-02-22 23:53:37 +0900

    [add] filesystem class

---
+++ lib/oceanus/utils/file_system.rb
@@ -0,0 +1,17 @@
+module Oceanus
+    module Utils
+        class FileSystem
+            # 論理ボリュームの作成
+            def create_volume
+            end
+
+            # スナップショットの作成
+            def create_snapshot
+            end
+
+            # 論理ボリュームの削除
+            def delete_volume
+            end
+        end
+    end
+end
