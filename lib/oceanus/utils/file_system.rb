73b03dd9a6bf79b8ba1012bbbda3a3705b77e688
timakin <timaki.st@gmail.com>
2016-02-23 23:43:13 +0900

    [fix] added btrfs cmd

--- lib/oceanus/utils/file_system.rb
+++ lib/oceanus/utils/file_system.rb
@@ -1,12 +1,19 @@
 module Oceanus
     module Utils
         class FileSystem
+            def initialize
+                @saving_path = "/var/oceanus"
+            end
             # 論理ボリュームの作成
-            def create_volume
+            def create_volume(uuid)
+                cmd = "btrfs subvolume create #{@saving_path}/#{uuid}"
+                exec(cmd)
             end
 
             # スナップショットの作成
-            def create_snapshot
+            def create_snapshot(image_id)
+                cmd = "btrfs subvolume snapshot #{@saving_path}/#{image_id} #{@saving_path}/#{uuid}"
+                exec(cmd)
             end
 
             # 論理ボリュームの削除
